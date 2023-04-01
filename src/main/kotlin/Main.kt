d63bfd05386815e2db1577be3c441d3b77520bbb
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-04-01 17:44:16 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+
+fun main(){
+    //string
+    var name = "janet"
+    println(name)
+    //byte
+    var age = 35
+    println(age)
+    //integers
+    var money = 56789089
+    println(money)
+    //short
+    var girls = 4565
+    println(girls)
+    //long
+    var amount = 56000000000
+    println(amount)
+    //double
+    var population = 3456.7897890
+    println(population)
+    //char
+    var letters = "h"
+    println(letters)
+    //Boolean
+    var receivedparcel = false
+    println(receivedparcel)
+    //array
+    var names = ("jane,joy,mary ")
+    println(names)
+    //infered types
+    val weight = 5.9
+    println(weight)
+//float
+    var classes = 23.789
+    println(classes)
+
+
+
+}
